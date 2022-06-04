ba4f91f36a80e82599d5182d236caba83a30bd1d
fatih zengin <fatihzengin654@outlook.com>
2022-06-04 22:49:30 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(100,500))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
